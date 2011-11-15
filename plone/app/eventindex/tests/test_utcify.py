d5b05339dbd2855e5f9530858c56a07045674e9c
Taito Horiuchi <taito.horiuchi@hexagonit.fi>
2011-11-15 15:42:47 +0200

    test.py moved to tests directry with name: test_EventIndexTests.py

---
+++ plone/app/eventindex/tests/test_utcify.py
@@ -0,0 +1,15 @@
+import mock
+import unittest2 as unittest
+
+
+class TestUtcify(unittest.TestCase):
+
+    def test_utificy(self):
+        from plone.app.eventindex.tests.test_EventIndexTests import utcify
+        from DateTime import DateTime
+        dt = mock.Mock(spec=DateTime)
+        utcify(dt)
+        self.assertTrue(dt.utcdatetime.called)
+        dt = mock.Mock()
+        utcify(dt)
+        self.assertTrue(dt.utctimetuple.called)
