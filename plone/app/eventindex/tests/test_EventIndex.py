a56bc1b89d0247f493935bcfd6d5936bc718f035
Taito Horiuchi <taito.horiuchi@hexagonit.fi>
2011-11-16 12:07:39 +0200

    module nema changed

--- plone/app/eventindex/tests/test_EventIndex.py
+++ plone/app/eventindex/tests/test_EventIndex.py
@@ -45,7 +45,7 @@ class TestEventIndex(unittest.TestCase):
         self.assertEqual(instance.manage.name(), 'manage_main')
 
     @mock.patch('plone.app.eventindex.EventIndex.clear')
-    def test_instance__init__attributes_without_extra(self, clear):
+    def test__init__attributes_without_extra(self, clear):
         instance = self.createInstance()
         self.assertEqual(instance._id, 'id')
         self.assertEqual(instance.start_attr, 'start')
@@ -54,7 +54,7 @@ class TestEventIndex(unittest.TestCase):
         self.assertTrue(clear.called)
 
     @mock.patch('plone.app.eventindex.EventIndex.clear')
-    def test_instance__init__attributes_with_extra(self, clear):
+    def test__init__attributes_with_extra(self, clear):
         extra = {
         }
         instance = self.createInstance(extra=extra)
@@ -86,3 +86,68 @@ class TestEventIndex(unittest.TestCase):
         self.assertEqual(instance.end_attr, 'END')
         self.assertEqual(instance.recurrence_attr, 'RECURRENCE')
         self.assertTrue(clear.called)
+
+    @mock.patch('plone.app.eventindex.IOBTree')
+    @mock.patch('plone.app.eventindex.OOBTree')
+    @mock.patch('plone.app.eventindex.Length')
+    def test_clear(self, Length, OOBTree, IOBTree):
+        instance = self.createInstance()
+        Length.reset_mock()
+        OOBTree.reset_mock()
+        IOBTree.reset_mock()
+        Length.return_value = 'length'
+        OOBTree.return_value = 'oobtree'
+        IOBTree.return_value = 'iobtree'
+        instance.clear()
+        self.assertEqual(Length.call_count, 1)
+        self.assertEqual(OOBTree.call_count, 2)
+        self.assertEqual(IOBTree.call_count, 4)
+        self.assertEqual(instance._length, 'length')
+        self.assertEqual(instance._end2uid, 'oobtree')
+        self.assertEqual(instance._start2uid, 'oobtree')
+        self.assertEqual(instance._uid2end, 'iobtree')
+        self.assertEqual(instance._uid2duration, 'iobtree')
+        self.assertEqual(instance._uid2start, 'iobtree')
+        self.assertEqual(instance._uid2recurrence, 'iobtree')
+
+    def test_getId(self):
+        instance = self.createInstance()
+        self.assertEqual(instance.getId(), 'id')
+
+    def test_getEntryForObject(self):
+        ## Should we implement something rather than raise NotImplementedError?
+        instance = self.createInstance()
+        self.assertRaises(
+            NotImplementedError,
+            lambda: instance.getEntryForObject(mock.Mock())
+        )
+
+    def test_getIndexSourceNames(self):
+        instance = self.createInstance()
+        instance.start_attr = 'start'
+        instance.end_attr = 'end'
+        instance.recurrence_attr = 'recurrence'
+        self.assertEqual(
+            instance.getIndexSourceNames(),
+            ('start', 'end', 'recurrence')
+        )
+
+    def test__getattr(self):
+        instance = self.createInstance()
+        ## Test with attribute
+        obj = mock.Mock()
+        obj.attr = 'ATTR'
+        self.assertEqual(instance._getattr('attr', obj), 'ATTR')
+        ## Test method but not DateTime
+        obj.method = mock.Mock(return_value='METHOD')
+        self.assertEqual(instance._getattr('method', obj), 'METHOD')
+        ## Test method which is DateTime
+        from DateTime import DateTime
+        dt = mock.Mock(spec=DateTime)
+        date_time = mock.Mock(return_value=dt)
+        date_time().utcdatetime.return_value = 'datetime'
+        obj.method = date_time
+        self.assertEqual(instance._getattr('method', obj), 'datetime')
+
+    def test_index_object(self):
+        instance = self.createInstance()
