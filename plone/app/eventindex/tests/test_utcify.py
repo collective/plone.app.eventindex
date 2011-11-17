e5179759272022752360a0881a68333266e3237a
Taito Horiuchi <taito.horiuchi@hexagonit.fi>
2011-11-17 15:20:48 +0200

    test updated

--- plone/app/eventindex/tests/test_utcify.py
+++ plone/app/eventindex/tests/test_utcify.py
@@ -5,7 +5,7 @@ import unittest2 as unittest
 class TestUtcify(unittest.TestCase):
 
     def test_utificy(self):
-        from plone.app.eventindex.tests.test_primal import utcify
+        from plone.app.eventindex.tests.test_original import utcify
         from DateTime import DateTime
         dt = mock.Mock(spec=DateTime)
         utcify(dt)
